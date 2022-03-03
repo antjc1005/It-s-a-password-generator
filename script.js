b3823945942f846076e2d8475b4abaedf2b67811
antjc1005 <99000478+antjc1005@users.noreply.github.com>
2022-03-02 22:14:18 -0800

    javanotrunning,debugger

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const empty="";
-const Ucase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const iCase="abcdefghijklmnopqrstuvwxyz";
+const uCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase="abcdefghijklmnopqrstuvwxyz";
 const number="01234567890";
 const symbol="!@#$%^&*-_+=";
 const plength=document.getElementById("p-length");
 const upperCase=document.getElementById("p-uppercase");
-const lowerCase=document.getElementById("p-lowerercase");
+const lowerCase=document.getElementById("p-lowercase");
 const pNumber=document.getElementById("p-number");
 const pSymbol=document.getElementById("p-symbol");
 const sumbit=document.getElementById("submit");
 const password=document.getElementById("password");
-
+//generate password click that runs function
 sumbit.addEventListener("click",()=>{
+    debugger;
     let initialPassword=empty;
-    (upperCase.checked)?initialPassword+=Ucase:"";
-    (lowerCase.checked)?initialPassword+=icase:"";
+    (upperCase.checked)?initialPassword+=uCase:"";
+    (lowerCase.checked)?initialPassword+=lCase:"";
     (pNumber.checked)?initialPassword+=number:"";
     (pSymbol.checked)?initialPassword+=symbol:"";
 
     password.value=generatePassword(plength.value,initialPassword)
 
 });
+//function to run password generator
 function generatePassword(I,initialPassword){
     let pass="";
     for(let i=0;i<I;i++){
@@ -29,3 +31,14 @@ function generatePassword(I,initialPassword){
     }
     return pass;
 }
+//copy
+const copy=document.getElementById("copybtn");
+copy.addEventListener("click", ()=>{
+    if(password.value == "") {
+        alert("Please generate a password")
+    }else {
+        password.select();
+        document.execCommand("copybtn");
+        alert("Password has been copied to clipboard");
+    }
+});
